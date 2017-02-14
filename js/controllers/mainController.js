69daed20b00ab818147116b34f6e8d50e1136952
JcDecour <doudouc27@gmail.com>
2017-02-14 13:49:15 +0100

    ajout du formulaire et tableaux

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,4 +1,11 @@
 // MAIN CONTROLLER
 function mainController($scope) {
-    
+  this.people = [];
+  this.submit = function(){
+     this.people.push({
+    "lastname":$scope.name,
+    "firstname":$scope.firstname,
+    "email":$scope.email
+  });
+}
 }
